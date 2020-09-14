76072ad6bd2b6f1de71d33edc834184cd99c7269
unknown <ivansudevlop@gmail.com>
2020-09-15 00:44:45 +0200

    To deploy

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -1,9 +1,9 @@
 import { useLayoutEffect } from "react";
 import Head from "next/head";
-import Styled from "./_404.styles";
 import NavLinks from "components/molecules/NavLinks";
 import { useTheme } from "styled-components";
 import { useLayoutStore } from "contexts/LayoutStore";
+import styled from "styled-components";
 
 const NotFound = () => {
   const theme = useTheme();
@@ -31,3 +31,40 @@ const NotFound = () => {
 };
 
 export default NotFound;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+const InnerWrapper = styled.div`
+  height: 400px;
+  margin-top: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Error = styled.h1`
+  font-size: 10em;
+  color: ${({ theme }) => theme.colors.light};
+  background-color: ${({ theme }) => theme.colors.dark};
+`;
+
+const Info = styled.h2`
+  font-size: 2.5em;
+  text-transform: capitalize;
+  padding: 5px 10px;
+  color: ${({ theme }) => theme.colors.light};
+  background-color: ${({ theme }) => theme.colors.primary};
+`;
+
+const Styled = {
+  Wrapper,
+  InnerWrapper,
+  Error,
+  Info,
+};
